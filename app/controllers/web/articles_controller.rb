2415d02521fe94ad00499b753f11df6e7e4cae6e
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-24 12:26:56 +0300

    article creation, updating, deleting

--- app/controllers/web/articles_controller.rb
+++ app/controllers/web/articles_controller.rb
@@ -1,4 +1,6 @@
 class Web::ArticlesController < Web::ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -6,4 +8,49 @@ class Web::ArticlesController < Web::ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.author = current_user
+
+    if @article.save
+      flash[:notice] = "Article successfully created"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully updated"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article successfully deleted"
+      redirect_to articles_path
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
 end
