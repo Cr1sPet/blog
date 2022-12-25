a3042fc8fe1f95e4474a2880cef2aa3a62fa196a
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-25 11:12:30 +0300

    user edit and user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   scope module: :web do
